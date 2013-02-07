c81f4226eb19a82b91774d4f8435407b1ac6223e
jagguli <steven@stevenjoseph.in>
2013-02-08 01:39:02 +1100

    disabled pudb

--- debug.py
+++ debug.py
@@ -8,9 +8,9 @@ ultratrace = ultratb.VerboseTB()
 # sys.excepthook = ultratrace
 from IPython.core.debugger import Tracer
 
-import pudb
-#debug = Tracer('Linux')
-debug = pudb.set_trace
+#import pudb
+#debug = pudb.set_trace
+debug = Tracer('Linux')
 
 
 def trace():
@@ -23,6 +23,26 @@ def trace_all():
     traceback.print_exception(exc_type, exc_value, exc_traceback)
     traceback.print_exception(exc_type, exc_value)
 
+
+# code snippet, to be included in 'sitecustomize.py'
+def info(type, value, tb):
+    if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+        # we are in interactive mode or we don't have a
+        # tty-like
+        # device, so we call the default hook
+        sys.__excepthook__(type, value, tb)
+    else:
+        # we are NOT in interactive mode, print the
+        # exception...
+        traceback.print_exception(type, value, tb)
+        print
+        # ...then start the debugger in
+        # post-mortem mode.
+        debug()
+
+
+def set_except_hook():
+    sys.excepthook = info
 # def trace_formatted() :
 #    exc_type , exc_value, exc_traceback = sys.exc_info()
 #    #print repr(traceback.format_exception(exc_type, exc_value, exc_traceback))
